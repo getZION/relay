7ecb8e0f8f3d581739a286fb076f107b351fbe33
Christopher David <chris@arcade.city>
2022-04-07 11:00:21 -0500

    Initial DWN types and parsing

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/getzion/relay/dwn"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
-
 	app.Post("/", func(c *fiber.Ctx) error {
+		// Require JSON
+		app.Use(func(c *fiber.Ctx) error {
+			if c.Is("json") {
+				return c.Next()
+			}
+			return c.SendString("JSON required.")
+		})
+
+		// Instantiate new Request struct
+		r := new(dwn.Request)
+		//  Parse body into Request struct
+		if err := c.BodyParser(r); err != nil {
+			log.Println(err)
+			c.Status(400).JSON(&fiber.Map{
+				"success": false,
+				"message": err,
+			})
+			return nil
+		}
+
 		return c.JSON(&fiber.Map{
-			"success": true,
+			"success":   true,
+			"requestId": r.RequestId,
+			"target":    r.Target,
 		})
 	})
 
