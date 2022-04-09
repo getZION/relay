9ab6f585c49cb0ddaf2c16a57ed092bfcb579474
Christopher David <chris@arcade.city>
2022-04-09 11:25:38 -0500

    Add storage and connect to main

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"os"
 
 	"github.com/getzion/relay/api/dwn/server"
+	"github.com/getzion/relay/api/models"
+	"github.com/getzion/relay/api/storage"
+	"github.com/getzion/relay/api/validator"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	server := server.InitDWNServer()
+	validator.InitValidator()
+
+	storage, err := storage.NewStorage("mysql")
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	modelManager := models.NewModelManager(storage)
+	server := server.InitDWNServer(modelManager, storage)
+
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
 	logrus.Fatal(server.Listen(fmt.Sprintf("%s:%s", host, port)))
