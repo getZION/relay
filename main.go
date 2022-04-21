8cb4f09d083c56de42454f310c1994cc8186c468
Christopher David <chris@arcade.city>
2022-04-21 17:04:57 -0400

    No bullshit

--- main.go
+++ main.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/getzion/relay/api/dwn/server"
-	"github.com/getzion/relay/api/models"
-	"github.com/getzion/relay/api/storage"
-	"github.com/getzion/relay/api/validator"
-	"github.com/sirupsen/logrus"
+	. "github.com/getzion/relay/utils"
 )
 
 func main() {
-	validator.InitValidator()
-
-	storage, err := storage.NewStorage("mysql")
-	if err != nil {
-		logrus.Panic(err)
-	}
-
-	modelManager := models.NewModelManager(storage)
-	server := server.InitDWNServer(modelManager, storage)
-
-	host := os.Getenv("HOST")
-	port := os.Getenv("PORT")
-	logrus.Fatal(server.Listen(fmt.Sprintf("%s:%s", host, port)))
+	Log.Debug().Msg("No bullshit.")
 }
