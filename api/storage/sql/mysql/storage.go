b1330562994c2dbfbdc1f287f9ed11ee975f8ed2
Christopher David <chris@arcade.city>
2022-04-21 17:37:06 -0400

    Axe manual migrations from mysql init

--- api/storage/sql/mysql/storage.go
+++ api/storage/sql/mysql/storage.go
@@ -6,12 +6,8 @@ import (
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/getzion/relay/api/storage/sql/common"
-	"github.com/getzion/relay/api/storage/sql/mysql/migrations"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-migrate/migrate/v4"
 
-	"github.com/golang-migrate/migrate/v4/database/mysql"
-	bindata "github.com/golang-migrate/migrate/v4/source/go_bindata"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 )
@@ -28,7 +24,6 @@ type mysqlStorage struct {
 }
 
 func NewMySqlStorage() (*mysqlStorage, error) {
-
 	var err error
 	var params mysqlConnectionParams
 	envconfig.Process("", &params)
@@ -43,34 +38,6 @@ func NewMySqlStorage() (*mysqlStorage, error) {
 
 	connection := mysqlStorage{
 		Connection: common.NewStore(db, sq.StatementBuilder.RunWith(sq.NewStmtCache(db))),
-	}
-
-	s := bindata.Resource(migrations.AssetNames(),
-		func(name string) ([]byte, error) {
-			return migrations.Asset(name)
-		})
-
-	driver, err := bindata.WithInstance(s)
-	if err != nil {
-		return nil, err
-	}
-
-	dbDriver, err := mysql.WithInstance(db, &mysql.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	m, err := migrate.NewWithInstance("go-bindata", driver, params.Name, dbDriver)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := m.Up(); err != nil {
-		if err.Error() == "no change" {
-			logrus.Info("no change made by migrations")
-		} else {
-			return nil, err
-		}
 	}
 
 	return &connection, nil
