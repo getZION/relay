a91e20eb2829bf5b1f1e691eea064bbd4cdaf4aa
Christopher David <chris@arcade.city>
2022-04-21 17:17:42 -0400

    Fiber app

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	. "github.com/getzion/relay/utils"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	Log.Debug().Msg("No bullshit.")
+	Log.Debug().Msg("Loading...")
+
+	app := fiber.New(fiber.Config{})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Relay is live!")
+	})
+	app.Listen(":8080")
 }
