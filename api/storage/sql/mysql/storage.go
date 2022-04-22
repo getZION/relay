8fec27f73034ce3533df933e87c9aad173308da1
Christopher David <chris@arcade.city>
2022-04-22 06:57:26 -0400

    initial gorm community test

--- api/storage/sql/mysql/storage.go
+++ api/storage/sql/mysql/storage.go
@@ -6,7 +6,6 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
-	"github.com/getzion/relay/api"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 )
@@ -32,10 +31,32 @@ func NewMySqlStorage() (*gorm.DB, error) {
 	}
 	logrus.Info("Connected to MySQL database.")
 
+	type CommunityORM struct {
+		// Conversations   []*ConversationORM `gorm:"foreignkey:community_zid;association_foreignkey:Zid"`
+		Created         int64  `gorm:"not null"`
+		Deleted         bool   `gorm:"default:false"`
+		Description     string `gorm:"size:250;not null"`
+		EscrowAmount    int64  `gorm:"not null"`
+		Id              int64  `gorm:"primary_key;unique"`
+		Img             string
+		LastActive      int64
+		Name            string `gorm:"size:150;unique;not null"`
+		OwnerDid        string `gorm:"not null"`
+		OwnerUsername   string `gorm:"not null"`
+		PricePerMessage int64  `gorm:"not null"`
+		PriceToJoin     int64  `gorm:"not null"`
+		Public          bool   `gorm:"default:true"`
+		// Tags            []*TagORM `gorm:"foreignkey:Id;association_foreignkey:Id;many2many:community_tags;jointable_foreignkey:CommunityId;association_jointable_foreignkey:TagId"`
+		Updated int64
+		// Users           []*UserORM `gorm:"foreignkey:Zid;association_foreignkey:Did;many2many:community_users;jointable_foreignkey:CommunityZid;association_jointable_foreignkey:UserDid"`
+		Zid string `gorm:"unique;not null"`
+	}
+
 	db.AutoMigrate(
-		&api.Community{},
-		&api.Conversation{},
-		&api.User{},
+		&CommunityORM{},
+	// &api.Community{},
+	// &api.Conversation{},
+	// &api.User{},
 	)
 	logrus.Info("Migrations successful.")
 
