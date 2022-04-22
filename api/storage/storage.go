04e97dcf33aa8077cc46377ea89e2aa81795c9ea
Christopher David <chris@arcade.city>
2022-04-22 06:25:14 -0400

    NewStorage cleanup

--- api/storage/storage.go
+++ api/storage/storage.go
@@ -6,21 +6,13 @@ import (
 	"gorm.io/gorm"
 )
 
-// NewStorage should use config options to return a connection to the requested database
 func NewStorage(storeType string) (storage *gorm.DB, err error) {
-
 	switch storeType {
 	case "mysql":
 		storage, err = mysql.NewMySqlStorage()
 	default:
-		logrus.Panic("No")
+		logrus.Panic("Unknown storage type")
 	}
-	// case "cache":
-	// 	storage, err = cache.NewStorage()
-	// default:
-	// 	logrus.Infof("unknown storage database: %s, cache storage activated", storeType)
-	// 	storage, err = cache.NewStorage()
-	// }
 
 	if err != nil {
 		return nil, err
