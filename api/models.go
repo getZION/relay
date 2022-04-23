74b1e81bd8db4e98f1d0b315e35c59a17e45c8b1
Christopher David <chris@arcade.city>
2022-04-23 16:51:08 -0400

    UserCommunity m2m test migrate

--- api/models.go
+++ api/models.go
@@ -12,6 +12,7 @@ type Community struct {
 	PricePerMessage int64  `json:"pricePerMessage" validate:"gte=0,lt=100000" gorm:"not null"`
 	PriceToJoin     int64  `json:"priceToJoin" validate:"gte=0,lt=100000" gorm:"not null"`
 	Updated         int64  `json:"updated,omitempty"`
+	Users           []User `json:"users,omitempty" gorm:"many2many:user_communities"`
 	Zid             string `json:"zid" gorm:"primary_key;unique;not null"`
 }
 
@@ -44,16 +45,17 @@ type Message struct {
 }
 
 type User struct {
-	Bio      string `json:"bio,omitempty"`
-	Created  int64  `json:"created,omitempty"`
-	Did      string `json:"did" validate:"required" gorm:"primary_key;unique;not null"`
-	Img      string `json:"img,omitempty"`
-	ImgCover string `json:"imgCover,omitempty"`
-	Link     string `json:"link,omitempty"`
-	Location string `json:"location,omitempty"`
-	Name     string `json:"name" validate:"required" gorm:"not null"`
-	Updated  int64  `json:"updated,omitempty"`
-	Username string `json:"username" validate:"required,username,min=6,max=16" gorm:"unique;not null"`
+	Bio         string      `json:"bio,omitempty"`
+	Communities []Community `json:"communities,omitempty" gorm:"many2many:user_communities"`
+	Created     int64       `json:"created,omitempty"`
+	Did         string      `json:"did" validate:"required" gorm:"primary_key;unique;not null"`
+	Img         string      `json:"img,omitempty"`
+	ImgCover    string      `json:"imgCover,omitempty"`
+	Link        string      `json:"link,omitempty"`
+	Location    string      `json:"location,omitempty"`
+	Name        string      `json:"name" validate:"required" gorm:"not null"`
+	Updated     int64       `json:"updated,omitempty"`
+	Username    string      `json:"username" validate:"required,username,min=6,max=16" gorm:"unique;not null"`
 }
 
 type Payment struct {
