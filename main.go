83e1c52df23cd48d7cb26b78e324035a9038a9c5
Christopher David <chris@arcade.city>
2022-04-25 10:26:20 -0400

    cleanup main.go

--- main.go
+++ main.go
@@ -19,11 +19,5 @@ func main() {
 	modelManager := models.NewModelManager(storage)
 	server := server.InitDWNServer(modelManager, storage)
 
-	// app := fiber.New(fiber.Config{})
-	// app.Get("/", func(c *fiber.Ctx) error {
-	// 	return c.SendString("Relay is live!")
-	// })
-	// app.Listen(":8080")
-
 	logrus.Fatal(server.Listen(":8080"))
 }
