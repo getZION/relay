c922563ca1526cd6d58f69d26db806732cfe97a5
Christopher David <chris@arcade.city>
2022-04-27 10:49:32 -0400

    AutoJoin lounge community

--- api/storage/sql/common/user.go
+++ api/storage/sql/common/user.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/getzion/relay/api"
@@ -36,6 +37,18 @@ func (c *Connection) InsertUser(user *api.User) error {
 	if result.Error != nil {
 		return result.Error
 	}
+
+	logrus.Infof("Created user. Attempting to join Lounge community...")
+	loungeZidCypher := "016cf037-6247-4b69-90e0-e83e2aad98fb"
+	community, err := c.GetCommunityByZid(loungeZidCypher)
+	if err != nil {
+		return fmt.Errorf("community not found: %s", loungeZidCypher)
+	}
+	err = c.AddUserToCommunity(community, user)
+	if err != nil {
+		return err
+	}
+	logrus.Infof("Success? %s", user.Did)
 
 	return nil
 }
