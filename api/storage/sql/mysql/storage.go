22fe69dcd571d2cfaaf5c1bb123314d603f25a45
Christopher David <chris@arcade.city>
2022-04-27 16:57:20 -0400

    disable gorm logging

--- api/storage/sql/mysql/storage.go
+++ api/storage/sql/mysql/storage.go
@@ -5,7 +5,6 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
 	"github.com/getzion/relay/api"
 	"github.com/getzion/relay/api/storage/sql/common"
@@ -32,7 +31,7 @@ func NewMySqlStorage() (*mysqlStorage, error) {
 	databaseConnectionString := fmt.Sprintf("%s:%s@tcp(%s:25060)/%s?multiStatements=true", params.User, params.Pass, params.Host, params.Name)
 
 	db, err := gorm.Open(mysql.Open(databaseConnectionString), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		// Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
