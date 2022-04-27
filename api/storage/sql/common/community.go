e82fcb36c39a54d06d24f8aa0e43a955cafe06d2
Christopher David <chris@arcade.city>
2022-04-27 16:31:10 -0400

    change error syntax

--- api/storage/sql/common/community.go
+++ api/storage/sql/common/community.go
@@ -50,8 +50,12 @@ func (c *Connection) AddUserToCommunity(community *api.Community, user *api.User
 	// c.db.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&user)
 
 	association := c.db.Model(community).Omit("Users").Association("Users").Append(user)
-	logrus.Info("AddUserToCommunity done maybe?!!!!!")
-	logrus.Info("AddUserToCommunity error?", association.Error())
+	if association != nil {
+		logrus.Info("AddUserToCommunity association error...", association.Error())
+	} else {
+		logrus.Info("AddUserToCommunity association seems to have no error...")
+	}
+
 	return association
 
 	// c.db.Session(&gorm.Session{FullSaveAssociations: true}).Updates(api.User{}).Updates(api.User{})
